9a3aeb0d0a614b8913678f7c094ea02ad89554e9
David Merino <david.merino.developer@gmail.com>
2023-11-29 13:02:36 +0100

    Add safe area from react native

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>JWGO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text>JWGO</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 })
